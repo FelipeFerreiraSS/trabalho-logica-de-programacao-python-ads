47332e555a7a8b4b3eb9a8e956c6e379ebab6d3d
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-07-30 23:26:57 -0300

    Create trabalho-criptografia.py

---
+++ trabalho-criptografia.py
@@ -0,0 +1,25 @@
+frase = input("Digite a frase")
+
+fraseArray = list(frase)
+
+tamanho = len(fraseArray)
+
+listaAscii = []
+
+for x in range(tamanho):
+    listaAscii.append(ord(fraseArray[x]))
+
+converteAscii = []
+
+for x in range(tamanho):
+    converteAscii.append(chr(listaAscii[x]))
+
+
+print("Frase inicial:")
+print(fraseArray)
+print("---------")
+print("Convertida em ascii")
+print(listaAscii)
+print("---------")
+print("Convertida de ascii")
+print(converteAscii)
