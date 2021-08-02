c1ba30c86f1ebd055e4c6a7b4294b5c14f126209
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-02 18:30:05 -0300

    Update trabalho-criptografia.py

--- trabalho-criptografia.py
+++ trabalho-criptografia.py
@@ -5,14 +5,16 @@ fraseArray = list(frase)
 tamanho = len(fraseArray)
 
 listaAscii = []
-
 for x in range(tamanho):
-    listaAscii.append(ord(fraseArray[x]))
+    listaAscii.append(ord(fraseArray[x])) #string para ascii
+
+binario = []
+for i in range(tamanho):
+    binario.append(format(listaAscii[i], "08b")) #scii para binario
 
 converteAscii = []
-
 for x in range(tamanho):
-    converteAscii.append(chr(listaAscii[x]))
+    converteAscii.append(chr(listaAscii[x]))  #de ascii para string
 
 
 print("Frase inicial:")
@@ -21,7 +23,14 @@ print("---------")
 print("Convertida em ascii")
 print(listaAscii)
 print("---------")
+print("De ascii para binario")
+print(binario)
+print("---------")
 print("Convertida de ascii")
 print(converteAscii)
+
+temp = "{0:b}".format(722)
+
+print(temp)
 
 
